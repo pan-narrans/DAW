43369d4cad32f498849daf33a0e1b8fb637de22a
alex <alexpalvarez@protonmail.com>
2021-11-30 11:36:40 +0100

    Exáment

--- Programacion/Examen_01.java
+++ Programacion/Examen_01.java
@@ -18,6 +18,9 @@ class Examen_01 {
     EMPATE, JUGADOR1, JUGADOR2
   };
 
+  /* ========================================================================== */
+  /* ============================= EJERCICIO 06.A ============================= */
+  /* ========================================================================== */
   /**
    * Ejercicio 6.a preguntará a un jugador humano que quiere jugar, piedra, papel
    * o tijera por teclado, después mediante operadores condicionales devolverá la
@@ -30,32 +33,35 @@ class Examen_01 {
     int manoInt;
     boolean manoLeida = false;
 
-    while (!manoLeida) {
+    do {
       manoLeida = true;
 
       System.out.println("Elige Piedra(1), Papel(2), o Tijera (3).");
       manoInt = sc.nextInt();
 
       switch (manoInt) {
-      case 1:
-        mano = Mano.PIEDRA;
-        break;
-      case 2:
-        mano = Mano.PAPEL;
-        break;
-      case 3:
-        mano = Mano.TIJERA;
-        break;
-      default:
-        manoLeida = false;
-        System.out.println("Valor introducido no válido. \n");
-        break;
+        case 1:
+          mano = Mano.PIEDRA;
+          break;
+        case 2:
+          mano = Mano.PAPEL;
+          break;
+        case 3:
+          mano = Mano.TIJERA;
+          break;
+        default:
+          manoLeida = false;
+          System.out.println("Valor introducido no válido. \n");
+          break;
       }
-    }
+    } while (!manoLeida);
 
     return mano;
   } // Fin jugadaHumano()
 
+  /* ========================================================================== */
+  /* ============================= EJERCICIO 06.B ============================= */
+  /* ========================================================================== */
   /**
    * Ejerciciio 6.b Esta función se utilizará para jugar contra la IA. En este
    * caso calculará un número aleatorio entre 0 y 2 con Math.random() * 3 y
@@ -70,21 +76,24 @@ class Examen_01 {
     manoInt = (int) (Math.random() * 3);
 
     switch (manoInt) {
-    case 0:
-      mano = Mano.PIEDRA;
-      break;
-    case 1:
-      mano = Mano.PAPEL;
-      break;
-    case 2:
-    default:
-      mano = Mano.TIJERA;
-      break;
+      case 0:
+        mano = Mano.PIEDRA;
+        break;
+      case 1:
+        mano = Mano.PAPEL;
+        break;
+      case 2:
+      default:
+        mano = Mano.TIJERA;
+        break;
     }
 
     return mano;
   } // Fin jugadaAleatoria()
 
+  /* ========================================================================== */
+  /* ============================= EJERCICIO 06.C ============================= */
+  /* ========================================================================== */
   /**
    * Ejercicio 6.c Esta función recibirá dos manos de dos jugadores, y devolverá
    * un tipo Ganador indicando si es un empate, si ha ganado el primer jugador o
@@ -111,6 +120,9 @@ class Examen_01 {
     return ganador;
   } // Fin ganador()
 
+  /* ========================================================================== */
+  /* ============================= EJERCICIO 06.D ============================= */
+  /* ========================================================================== */
   /**
    * Ejercicio 6.d Esta función permite jugar una partida contra la IA. Debe
    * implementar un bucle para repetir partidas hasta que el jugador ya no desee
@@ -142,6 +154,9 @@ class Examen_01 {
 
   } // Fin juegoPiedraPapelTijera()
 
+  /* ========================================================================== */
+  /* ============================== EJERCICIO 05 ============================== */
+  /* ========================================================================== */
   /**
    * Ejercicio 05 Implementar en la función main el siguiente menú y ejecute las
    * acciones correspondientes: Calcula la cuota de socio de un club de padel
@@ -164,54 +179,58 @@ class Examen_01 {
       System.out.println("");
 
       switch (ejercicio) {
-      case 1:
-        cuotaSocio();
-        break;
-      case 2:
-        int hora, minutos, segundos;
+        case 1:
+          cuotaSocio();
+          break;
+        case 2:
+          int hora, minutos, segundos;
 
-        System.out.println("Función para comprobar si una hora dada es válida o no:");
-        System.out.println("Introduce la hora, minutos y segundos por separado y en ese orden:");
-        hora = sc.nextInt();
-        minutos = sc.nextInt();
-        segundos = sc.nextInt();
+          System.out.println("Función para comprobar si una hora dada es válida o no:");
+          System.out.println("Introduce la hora, minutos y segundos por separado y en ese orden:");
+          hora = sc.nextInt();
+          minutos = sc.nextInt();
+          segundos = sc.nextInt();
 
-        System.out.println("La hora introducida " + (horaValida(hora, minutos, segundos) ? " " : "no ") + "es válida.");
-        break;
-      case 3:
-        int n;
-        long factorial;
+          System.out
+              .println("La hora introducida " + (horaValida(hora, minutos, segundos) ? " " : "no ") + "es válida.");
+          break;
+        case 3:
+          int n;
+          long factorial;
 
-        System.out.println("Función para calcular el factorial de un número:");
-        System.out.println("Introduce el número:");
-        n = sc.nextInt();
+          System.out.println("Función para calcular el factorial de un número:");
+          System.out.println("Introduce el número:");
+          n = sc.nextInt();
 
-        factorial = factorial(n);
-        System.out.println(factorial == -1 ? "El número introducido no es válido."
-            : "El factorial de " + n + " es: " + factorial + ".");
-        break;
-      case 4:
-        int numero;
+          factorial = factorial(n);
+          System.out.println(factorial == -1 ? "El número introducido no es válido."
+              : "El factorial de " + n + " es: " + factorial + ".");
+          break;
+        case 4:
+          int numero;
 
-        System.out.println("Función para calcular la suma de los dígitos de un número:");
-        System.out.println("Introduce el número:");
-        numero = sc.nextInt();
+          System.out.println("Función para calcular la suma de los dígitos de un número:");
+          System.out.println("Introduce el número:");
+          numero = sc.nextInt();
 
-        System.out.println("La suma de los dígitos de " + numero + " es: " + calculaSumaDigitos(numero) + ".");
-        break;
-      case 5:
-        System.out.println("¡Nos vemos luego!");
-        mostrarMenu = false;
-        break;
-      default:
-        System.out.println("Valor introducido no válido.");
-        break;
+          System.out.println("La suma de los dígitos de " + numero + " es: " + calculaSumaDigitos(numero) + ".");
+          break;
+        case 5:
+          System.out.println("¡Nos vemos luego!");
+          mostrarMenu = false;
+          break;
+        default:
+          System.out.println("Valor introducido no válido.");
+          break;
       }
 
     } while (mostrarMenu);
 
   } // Fin menu()
 
+  /* ========================================================================== */
+  /* ============================== EJERCICIO 04 ============================== */
+  /* ========================================================================== */
   /**
    * Ejercicio 04 Suma los dígitos de un número.
    * 
@@ -232,6 +251,9 @@ class Examen_01 {
     return result;
   } // Fin calculaSumaDigitos()
 
+  /* ========================================================================== */
+  /* ============================== EJERCICIO 03 ============================== */
+  /* ========================================================================== */
   /**
    * Ejercicio 03 Completar la función static long factorial (int n) que recibe un
    * número y devuelve el factorial. Deberá comprobar que el número que recibe sea
@@ -244,7 +266,7 @@ class Examen_01 {
   static long factorial(int n) {
     long result = 1;
 
-    if (n < 0 | n > 20) {
+    if (n < 0 | n > 20) { // !(-1 < n && n < 21)
       result = -1;
     } else {
       // Empezamos en 2 porque 0! y 1! son iguales a 1, por lo que no hace falta
@@ -257,11 +279,14 @@ class Examen_01 {
     return result;
   } // Fin factorial()
 
+  /* ========================================================================== */
+  /* ============================== EJERCICIO 02 ============================== */
+  /* ========================================================================== */
   /**
    * Ejercicio 02 Comprueba que todos los parámetros de la hora son válidos.
    * Supondremos que se lee la hora en modo 24 Horas
    * 
-   * @param hora     Entre 0 a 24
+   * @param hora     Entre 0 a 24 (23!!!)
    * @param minutos  Entre 0 y 59
    * @param segundos Entre 0 y 59
    * @return Devuelve si la hora es válida
@@ -269,19 +294,28 @@ class Examen_01 {
   static boolean horaValida(int hora, int minutos, int segundos) {
     boolean valida = true;
 
-    if (hora > 24 | hora < 0) {
+    // HORA > 23 !!!
+    if (hora > 24 || hora < 0) {
       valida = false;
-    } else if (minutos > 59 | minutos < 0) {
+    } else if (minutos > 59 || minutos < 0) {
       valida = false;
-    } else if (segundos > 59 | segundos < 0) {
+    } else if (segundos > 59 || segundos < 0) {
       valida = false;
     } else {
       ;
     }
 
-    return valida;
+    // Otra opción
+    return (hora > -1 && hora < 24 &&
+        minutos > -1 && minutos < 60 &&
+        segundos > -1 && segundos < 60);
+
+    // return valida;
   } // Fin horaValida()
 
+  /* ========================================================================== */
+  /* ============================== EJERCICIO 01 ============================== */
+  /* ========================================================================== */
   /**
    * Ejercicio 01 calcula la cuota anual que se debe abonar en un club de padel.
    * La cuota general es de 800 euros. Pedirá por teclado la edad del usuario y en
@@ -296,6 +330,9 @@ class Examen_01 {
     boolean hijoDeSocio;
     int edad;
     float cuota = 800;
+ 
+    // BUENA PRACTICA
+    float descuentoMayores = 0.4, descuentoJovenHijoSocio = 0.45, desjuentoJoven = 0.25;
 
     // Entrada de usuario
     System.out.println("Bienvenid@ al calculador de cuota del club de padel.");
@@ -305,9 +342,11 @@ class Examen_01 {
     // Calcular la cuota correspondiente
     if (edad < 21) {
       System.out.println("¿Es alguno de tus padres socio del club? \n 0. no \n 1. si");
+      // Para leer s/n -> sc.Next().toLowerCase().chartAt(0);
       hijoDeSocio = sc.nextInt() == 1 ? true : false;
       cuota = hijoDeSocio ? cuota * 0.55f : cuota * 0.75f;
     } else if (edad > 64) {
+      // cuota = cuota * (1 - descuentoMayores);
       cuota *= 0.6f;
     } else {
       ;
@@ -319,6 +358,9 @@ class Examen_01 {
 
   } // Fin cuentaSocio()
 
+  /* ========================================================================== */
+  /* ================================== MAIN ================================== */
+  /* ========================================================================== */
   public static void main(String[] args) {
 
     menu();
