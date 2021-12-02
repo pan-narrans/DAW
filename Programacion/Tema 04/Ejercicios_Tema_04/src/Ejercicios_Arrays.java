06ac98424623a1a3ca6ee9620ff1cc7b77dde8e2
alex <alexpalvarez@protonmail.com>
2021-12-02 10:59:17 +0100

    Ejercicios tema 04 de arrays avanzados.

--- Programacion/Tema 04/Ejercicios_Tema_04/src/Ejercicios_Arrays.java
+++ Programacion/Tema 04/Ejercicios_Tema_04/src/Ejercicios_Arrays.java
@@ -3,26 +3,187 @@ import java.util.Arrays;
 
 public class Ejercicios_Arrays {
   static Scanner sc = new Scanner(System.in);
-  /*
+
+  /* ========================================================================== */
+  /* ============================== EJERCICIO 08 ============================== */
+  /* ========================================================================== */
+  /**
+   * <h5>EJERCICIO 08</h5>
+   * Escribe la función void busquedaNumeros(int [] numeros) que pedirá al usuario
+   * buscar un número introducido por teclado y le dirá si ha sido encontrado o
+   * no.
+   * <p>
+   * En el caso en el que haya sido encontrado, le indicará cuántas veces se
+   * ha introducido.
+   * <p>
+   * El programa preguntará si quiere seguir buscando más números
+   * o no: “¿Desea seguir buscando?(s/n)”.
    * 
+   * @param numeros
+   */
+  void busquedaNumeros(int[] numeros) {
+    boolean seguirBuscando = false;
+
+    do {
+      imprimirArray(numeros);
+      System.out.println("===========================");
+      System.out.println("¿Qué número quieres buscar?");
+      int numero = sc.nextInt();
+      int apariciones = aparicionesNumero(numeros, numero);
+
+      if (apariciones > 0) {
+        System.out.println("Apariciones del número: " + apariciones);
+      } else {
+        System.out.println("¡El número no aparece ni una sola vez!");
+      }
+
+      System.out.println("¿Quieres seguir buscando? s/n");
+      char respuesta = sc.next().charAt(0);
+      seguirBuscando = (java.lang.Character.toLowerCase(respuesta) == 's') ? true : false;
+    } while (seguirBuscando);
+  }
+
+  /* ========================================================================== */
+  /* ============================== EJERCICIO 07 ============================== */
+  /* ========================================================================== */
+  /**
+   * <h5>EJERCICIO 07</h5>
+   * Escriba la función int aparicionesNumero(int [] numeros, int numero) que
+   * devuelve el número de veces que aparece numero en el array.
    * 
+   * @param numeros Array de integers a comprobar.
+   * @param numero  Integer a buscar en el array.
+   * @return Número de veces que aparece número en el array.
+   */
+  int aparicionesNumero(int[] numeros, int numero) {
+    int apariciones = 0;
+
+    for (int i : numeros) {
+      if (i == numero)
+        apariciones++;
+    }
+
+    return apariciones;
+  }
+
+  /* ========================================================================== */
+  /* ============================== EJERCICIO 06 ============================== */
+  /* ========================================================================== */
+  /**
+   * <h5>EJERCICIO 06</h5>
+   * Realizar la función String [][] primitiva () Devuelve una matriz con la
+   * apuesta aleatoria (seis números enteros aleatorios escogidos entre 1 y 49).
+   * <p>
+   * La matriz deberá contener una matriz de 10 x 40, con los valores que le
+   * corresponde, sustituyendo los que forman parte de la apuesta por una “X” y
+   * usando el formato de los boletos oficiales impresos.
+   */
+
+  String[][] primitiva() {
+    String[][] primitiva = new String[10][40];
+
+    // Rellenar la primitiva con cada una de las apuestas individuales
+    for (int i = 0; i < 40 / 5; i++) {
+      String[][] apuesta = setPartePrimitiva();
+      apuesta(apuesta);
+      for (int j = i * 5; j < i * 5 + 5; j++) {
+        for (int j2 = 0; j2 < 10; j2++) {
+          primitiva[j2][j] = apuesta[j2][j - i * 5];
+        }
+      }
+    }
+
+    return primitiva;
+  }
+
+  void imprimirPrimitiva(String[][] primitiva) {
+    int caja = 0;
+
+    // Imprimir parte de arriba
+    for (int i = 0; i < 7; i++) {
+      System.out.print("+---------------");
+    }
+    System.out.println("+---------------+");
+
+    // Imprimir valores
+    for (String[] strings : primitiva) {
+      System.out.print("| ");
+      for (String string : strings) {
+        System.out.print(string + ' ' + ((++caja % 5 == 0) ? "| " : ""));
+      }
+      System.out.println("");
+    }
+
+    // Imprimir parte de abajo
+    for (int i = 0; i < 7; i++) {
+      System.out.print("+---------------");
+    }
+    System.out.println("+---------------+");
+  }
+
+  void apuesta(String[][] primitiva) {
+    int row = 0;
+    int column = 0;
+
+    // Evita que una apuesta caiga en el [0][0]
+    while (row == column && row == 0) {
+      row = (int) (Math.random() * primitiva.length);
+      column = (int) (Math.random() * primitiva[0].length);
+    }
+
+    primitiva[row][column] = (column > 0) ? "X " : "X";
+  }
+
+  String[][] setPartePrimitiva() {
+    String[][] primitiva = new String[10][5];
+    int number = 0;
+
+    for (int i = 0; i < primitiva[0].length; i++) {
+      for (int j = 0; j < primitiva.length; j++) {
+        primitiva[j][i] = Integer.toString(number++);
+      }
+    }
+
+    return primitiva;
+  }
+
+  /* ========================================================================== */
+  /* ============================== EJERCICIO 05 ============================== */
+  /* ========================================================================== */
+  /**
+   * <h5>EJERCICIO 05</h5>
    * Realizar la función int [] primitiva() que genere una primitiva de 6 números
    * enteros entre el 1 y el 49.
    * 
+   * @return
    */
+  int[] apuestaPrimitiva() {
+    int[] primitiva = new int[6];
+
+    for (int i = 0; i < primitiva.length; i++) {
+      primitiva[i] = (int) (Math.random() * 48) + 1;
+    }
+
+    return primitiva;
+  }
 
   /* ========================================================================== */
   /* ============================== EJERCICIO 04 ============================== */
   /* ========================================================================== */
   /**
    * <h5>EJERCICIO 04</h5>
-   * Realizar la función  {char [] quiniela()} que genere una quiniela de 15
+   * Realizar la función {char [] quiniela()} que genere una quiniela de 15
    * resultados con valores aleatorios 1, X o 2.
    * 
    * @return
    */
   char[] quiniela() {
-    char[] quiniela = new char[14];
+    char[] quiniela = new char[15];
+
+    for (int i = 0; i < quiniela.length; i++) {
+      int aux = (int) (Math.random() * 3);
+      quiniela[i] = (aux == 0 ? '1' : (aux == 1) ? 'X' : '2');
+    }
 
     return quiniela;
   }
@@ -69,6 +230,20 @@ public class Ejercicios_Arrays {
    */
   void imprimirArray(int[] numeros) {
     System.out.println("Array = " + Arrays.toString(numeros));
+  }
+
+  void imprimirArray(char[] chars) {
+    System.out.println("Array = " + Arrays.toString(chars));
+  }
+
+  void imprimirArray(String[][] stringMatrix) {
+    System.out.println("Primitiva = ");
+    for (String[] strings : stringMatrix) {
+      for (String string : strings) {
+        System.out.print(string + ' ');
+      }
+      System.out.println();
+    }
   }
 
   /* ========================================================================== */
--- Programacion/Tema 04/Ejercicios_Tema_04/src/Main.java
+++ Programacion/Tema 04/Ejercicios_Tema_04/src/Main.java
@@ -1,13 +1,81 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) throws Exception {
 
+    Scanner sc = new Scanner(System.in);
     Ejercicios_Arrays ej1 = new Ejercicios_Arrays();
+    boolean correrEjercicios = true;
 
-    int[] numeros = new int[4];
-    int[] pares = { 23, 2, 23, 4, 2, 35, 34, 5, 23, 3, 4, 1, 2, 5, 1, 2, 34 };
-    // ej1.rellenarArray(numeros);
-    // ej1.imprimirArray(numeros);
+    int[] pares = { 23, 2, 23, 4, 2, 35, 34, 5, 23, 34, 2, 24, 5, 3, 4, 1, 2, 5, 1, 2, 34 };
 
-    ej1.imprimirArray(ej1.filtrarPares(pares));
+    do {
+      System.out.println("\n==================");
+      System.out.println("Ejercicios Arrays:");
+      System.out.println("    03: Filtrar Pares");
+      System.out.println("    04: Quiniela");
+      System.out.println("    05: Apuesta Primitiva");
+      System.out.println("    06: Primitiva");
+      System.out.println("    07: Apariciones de número en array");
+      System.out.println("    08: Apariciones de número en array (bis)");
+      System.out.println("    -1: Salir \n");
+
+      System.out.println("¿Qué ejercicio quieres probar?");
+      int ejercicio = sc.nextInt();
+
+      switch (ejercicio) {
+        case 3:
+          System.out.println("\nEjercicio 03: Filtrar Pares");
+          ej1.imprimirArray(pares);
+          ej1.imprimirArray(ej1.filtrarPares(pares));
+          break;
+
+        case 4:
+          System.out.println("\nEjercicio 04: Quiniela");
+          ej1.imprimirArray(ej1.quiniela());
+          break;
+
+        case 5:
+          System.out.println("\nEjercicio 05: Apuesta Primitiva");
+          ej1.imprimirArray(ej1.apuestaPrimitiva());
+          break;
+
+        case 6:
+          System.out.println("\nEjercicio 06: Primitiva");
+          ej1.imprimirPrimitiva(ej1.primitiva());
+          break;
+
+        case 7:
+          System.out.println("\nEjercicio 07: Apariciones de número en array");
+          int numeroABuscar = 4;
+          int apariciones = ej1.aparicionesNumero(pares, numeroABuscar);
+          ej1.imprimirArray(pares);
+          System.out
+              .print("En el array hay \'" + apariciones + ((apariciones == 1) ? "\' aparición" : "\' apariciones"));
+          System.out.println(" de el número " + numeroABuscar + ".");
+          break;
+
+        case 8:
+          System.out.println("\nEjercicio 08: Apariciones de número en array (bis)");
+          ej1.busquedaNumeros(pares);
+          break;
+
+        case 9:
+          break;
+
+        case -1:
+        default:
+          System.out.println("¡Adiós!");
+          correrEjercicios = false;
+          break;
+      }
+      if (correrEjercicios) {
+        System.out.println("Pulsa enter para continuar.");
+        System.in.read();
+      }
+
+    } while (correrEjercicios);
+
+    sc.close();
   }
 }
