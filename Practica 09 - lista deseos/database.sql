7362d8bc32c943c6beae2838bfc9e1483b30ba8c
alex <alexpalvarez@protonmail.com>
2022-01-10 10:15:33 +0100

    Reyes magos casi acabada - Deseos empezada

---
+++ Practica 09 - lista deseos/database.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS form_deseos;
+CREATE DATABASE form_deseos CHARACTER SET utf8mb4;
+USE form_deseos;
+
+CREATE TABLE form(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+
+  nombre VARCHAR(200) NOT NULL,
+  direccion VARCHAR(200) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+
+  localidad VARCHAR(100) NOT NULL,
+  provincia VARCHAR(100) NOT NULL,
+
+  deseos VARCHAR(1000) NOT NULL,
+  seguimiento BOOLEAN DEFAULT FALSE
+
+);
