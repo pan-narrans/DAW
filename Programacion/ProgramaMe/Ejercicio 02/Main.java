d217b2f7147a89cd36781c532b8359cfc47c1c97
Alex <alexpalvarez@protonmail.com>
2022-01-12 20:47:08 +0100

    Minor changes

--- Programacion/ProgramaMe/Ejercicio 02/Main.java
+++ Programacion/ProgramaMe/Ejercicio 02/Main.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Main {
 
@@ -5,33 +7,46 @@ public class Main {
 
   public static void main(String args[]) {
     in = new java.util.Scanner(System.in);
-    while (intento2())
-      ;
+    /*
+     * while (intento2())
+     * ;
+     */
+
+    Date fecha = new Date();
+    fecha.setTime(90000000000000000L);
+    System.out.println(fecha);
+
+    //GregorianCalendar fechaNacimiento = new GregorianCalendar(1996, 5, 9);
+    //GregorianCalendar fechaActual = new GregorianCalendar(1996, 5, 9);
+    //System.out.println(fechaNacimiento.get(fechaNacimiento.DAY_OF_MONTH) + "/" + fechaNacimiento.get(fechaNacimiento.MONTH) + "/" + fechaNacimiento.get(fechaNacimiento.YEAR));
   }
 
   public static boolean intento2() {
-    int diaN = in.nextInt();
-    int mesN = in.nextInt();
-    int yearN = in.nextInt();
+    int birthDay = in.nextInt();
+    int birthMonth = in.nextInt();
+    int birthYear = in.nextInt();
     int diaA = in.nextInt();
     int mesA = in.nextInt();
     int yearA = in.nextInt();
     int total = 1;
 
-    if (diaN == 0 && diaA == 0 && mesN == 0 && mesA == 0 && yearN == 0 && yearA == 0) {
+    GregorianCalendar fechaNacimiento = new GregorianCalendar(birthDay, birthMonth, birthYear);
+    System.out.println(fechaNacimiento);
+
+    if (birthDay == 0 && diaA == 0 && birthMonth == 0 && mesA == 0 && birthYear == 0 && yearA == 0) {
       return false;
-    } else if (mesN == mesA && diaN == diaA) { // Es nuestro cumple
+    } else if (birthMonth == mesA && birthDay == diaA) { // Es nuestro cumple
       System.out.println(0);
       return true;
     }
 
-    if (mesN <= mesA && diaN < diaA) { // Ha sido nuestro cumple
+    if (birthMonth <= mesA && birthDay < diaA) { // Ha sido nuestro cumple
       total--;
     }
 
-    total += daysTillNewYear(mesN, diaN);
+    total += daysTillNewYear(birthMonth, birthDay);
     total -= daysTillNewYear(mesA, diaA);
-    total += ((yearA - yearN) * 364);
+    total += ((yearA - birthYear) * 364);
 
     System.out.println(total);
 
--- Programacion/ProgramaMe/Ejercicio 06/Main.java
+++ Programacion/ProgramaMe/Ejercicio 06/Main.java
@@ -1,52 +1,37 @@
 import java.util.Arrays;
+import java.util.Locale;
 
 public class Main {
 
   static java.util.Scanner in;
 
   public static boolean casoDePrueba() {
-    /*
-     * int n;
-     * n = in.nextInt();
-     */
 
-    /*
-     * if (!in.hasNext()) // Exit condition
-     * return false;
-     */
+    if (!in.hasNext()) // Exit condition
+      return false;
 
     float[] notas = new float[7];
 
     for (int i = 0; i < 7; i++) {
       float nota = in.nextFloat();
       notas[i] = nota;
-      System.out.println(notas[i]);
-
     }
 
     Arrays.sort(notas);
 
     float suma = (notas[2] + notas[3] + notas[4]) * 2;
 
-    System.out.println((int) (suma));
+    System.out.println((int) suma);
 
     return true;
   }
 
   public static void main(String args[]) {
-    in = new java.util.Scanner(System.in);
-    /*
-     * while (casoDePrueba())
-     * ;
-     */
-    // casoDePrueba();
-    test();
-  }
+    in = new java.util.Scanner(System.in).useLocale(Locale.US);;
 
-  public static void test() {
-    float number;
-    number = in.nextFloat();
-    System.out.println("el numero es " + number);
+    while (casoDePrueba())
+      ;
+
   }
 
 }
