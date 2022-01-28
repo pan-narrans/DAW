f0e7a661d39cedcebda7a1d35d95f5a643bd52ee
alex <alexpalvarez@protonmail.com>
2022-01-28 12:20:47 +0100

    Ejemplos

--- BBDD/1-1-7 ejercicios.sql
+++ BBDD/1-1-7 ejercicios.sql
@@ -31,10 +31,11 @@ SELECT p1.nombre AS "Producto más caro de Lenovo" FROM producto p1
 ;
 
 -- 4. Lista el nombre del producto más barato del fabricante Hewlett-Packard.
-SELECT p1.nombre AS "Producto más barato de Lenovo" FROM producto p1
+SELECT p1.nombre AS "Producto más barato de Hewlett-Packard" 
+  FROM producto p1
   WHERE p1.codigo_fabricante = 
     (SELECT f1.codigo FROM fabricante f1 
-    WHERE UPPER(f1.nombre) LIKE "LENOVO")
+    WHERE UPPER(f1.nombre) LIKE "HEWLETT-PACKARD")
   ORDER BY p1.precio ASC LIMIT 1
 ;
 
@@ -48,7 +49,7 @@ SELECT p1.nombre as "Productos => caros que el producto más caro de Lenovo" FRO
 ;
 
 -- 6. Lista todos los productos del fabricante Asus que tienen un precio superior al precio medio de todos sus productos.
-SELECT p1.nombre AS "Productos de Lenovo" FROM producto p1
+SELECT p1.nombre AS "Productos de Asus con precio superior a la media" FROM producto p1
   WHERE p1.codigo_fabricante = 
     (SELECT fab.codigo FROM fabricante fab 
     WHERE UPPER(fab.nombre) LIKE "ASUS")
@@ -58,3 +59,18 @@ SELECT p1.nombre AS "Productos de Lenovo" FROM producto p1
       (SELECT fab.codigo FROM fabricante fab 
       WHERE UPPER(fab.nombre) LIKE "ASUS"))
 ;
+
+
+
+-- ##############################################
+-- 1.1.7.4 Subconsultas con EXISTS y NOT EXISTS
+-- ##############################################
+
+-- 14. Devuelve los nombres de los fabricantes que tienen productos asociados. (Utilizando EXISTS o NOT EXISTS).
+SELECT * FROM fabricante f1 WHERE EXISTS 
+  (SELECT * FROM producto p1 WHERE p1.codigo_fabricante = f1.codigo);
+
+-- 15. Devuelve los nombres de los fabricantes que no tienen productos asociados. (Utilizando EXISTS o NOT EXISTS)
+SELECT * FROM fabricante f1 WHERE NOT EXISTS 
+  (SELECT * FROM producto p1 WHERE p1.codigo_fabricante = f1.codigo);
+
