b9a1c85430a9051acdde381e4fadd4a87a596c3e
alex <alexpalvarez@protonmail.com>
2022-02-09 13:39:23 +0100

    BBDD: añadido ejemplo de subconsultas, falta meterlo en lecciones

--- BBDD/1-4-8-2 all y any.sql
+++ BBDD/1-4-8-2 all y any.sql
@@ -19,12 +19,37 @@ SELECT p1.nombre FROM producto p1
 
 -- Devuelve las oficinas donde no trabajan ninguno de los empleados que hayan sido los representantes de ventas de algún cliente que haya realizado la compra de algún producto de la gama Frutales.
 
-SELECT ofi1.codigo_oficina FROM oficina ofi1
-  INNER JOIN empleado emp1 ON emp1.codigo_oficina = ofi1.codigo_oficina
-  INNER JOIN cliente cli1 ON cli1.codigo_empleado_rep_ventas = emp1.codigo_empleado
-  INNER JOIN pedido ped1 ON ped1.codigo_cliente = cli1.codigo_cliente
-  INNER JOIN detalle_pedido det1 ON det1.codigo_pedido = ped1.codigo_pedido
-  INNER JOIN producto prod1 ON prod1.codigo_producto = det1.codigo_producto
+SELECT ofi1.codigo_oficina, ofi1.ciudad FROM oficina ofi1
+  -- Sacamos los que tienen frutales y nos aseguramos de no coger esos
+  WHERE ofi1.codigo_oficina NOT IN (
+    SELECT  emp1.codigo_oficina FROM empleado emp1
+    INNER JOIN cliente cli1 ON cli1.codigo_empleado_rep_ventas = emp1.codigo_empleado
+    INNER JOIN pedido ped1 ON ped1.codigo_cliente = cli1.codigo_cliente
+    INNER JOIN detalle_pedido det1 ON det1.codigo_pedido = ped1.codigo_pedido
+    INNER JOIN producto prod1 ON prod1.codigo_producto = det1.codigo_producto
+    WHERE UPPER(prod1.gama) LIKE "FRUTALES"
+  )
+  GROUP BY ofi1.codigo_oficina
+  ORDER BY 1;
 
-  WHERE UPPER(prod1.gama) NOT LIKE "FRUTALES"
-  GROUP BY ofi1.codigo_oficina 
+
+
+SELECT ofi1.codigo_oficina, ofi1.ciudad FROM oficina ofi1
+  WHERE ofi1.codigo_oficina NOT IN (
+  SELECT emp1.codigo_oficina FROM empleado emp1  
+  WHERE emp1.codigo_empleado IN(
+    SELECT cli1.codigo_empleado_rep_ventas FROM cliente cli1 
+    WHERE cli1.codigo_cliente IN(
+      SELECT ped1.codigo_cliente FROM pedido ped1
+      WHERE ped1.codigo_pedido IN(
+        SELECT det1.codigo_pedido FROM detalle_pedido det1
+        WHERE det1.codigo_producto IN(
+          SELECT prod1.codigo_producto FROM producto prod1
+          WHERE UPPER(prod1.gama) LIKE "FRUTALES"
+          )
+        )
+      )
+    )
+  )
+  ORDER BY 1;
+
